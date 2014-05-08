f30b419896315122e2a2a02ccb9805bc42dd8c3a
root <root@shujinko.local>
2014-05-08 15:33:22 -0700

    move to hs color scheme and factor mode into saturation

--- hue-for-squeezebox.rb
+++ hue-for-squeezebox.rb
@@ -4,23 +4,21 @@ require 'echowrap'
 require 'hue'
 require 'squeezer'
 
-# Color mappings based off Newton's color wheel
-# http://tomellard.com/wp/wp-content/uploads/2014/01/Newtons-colour-wheel.jpg
+# Color mappings based off http://wagneric.com/audiocolors.html
 # (c, c-sharp, d, e-flat, e, f, f-sharp, g, a-flat, a, b-flat, b) 0 - 11
-# array is xy color
 colors = {
-  0 => {:x => 0.15914, :y => 0.09869},
-  1 => {:x => 0.22430, :y => 0.12514},
-  2 => {:x => 0.31838, :y => 0.17666},
-  3 => {:x => 0.42430, :y => 0.23873},
-  4 => {:x => 0.56316, :y => 0.33393},
-  5 => {:x => 0.46303, :y => 0.43574},
-  6 => {:x => 0.41747, :y => 0.49176},
-  7 => {:x => 0.36042, :y => 0.53709},
-  8 => {:x => 0.31834, :y => 0.56770},
-  9 => {:x => 0.27491, :y => 0.45559},
-  10 => {:x => 0.23000, :y => 0.34380},
-  11 => {:x => 0.20568, :y => 0.25731}
+  0 => {:hue => 20206, :bri => 254},
+  1 => {:hue => 32221, :bri => 254},
+  2 => {:hue => 38410, :bri => 254},
+  3 => {:hue => 43872, :bri => 254},
+  4 => {:hue => 46966, :bri => 222},
+  5 => {:hue => 49697, :bri => 120},
+  6 => {:hue => 0, :bri => 173},
+  7 => {:hue => 0, :bri => 254},
+  8 => {:hue => 0, :bri => 254},
+  9 => {:hue => 4369, :bri => 254},
+  10 => {:hue => 10194, :bri => 254},
+  11 => {:hue => 15291, :bri => 254}
 }
 
 # Squeeze config
@@ -57,14 +55,20 @@ while true do
 
       song = Squeezer::Models::Model.extract_records(Squeezer::Connection.exec("#{squeeze.players.first.id} status -")).first
   
-      # Return key for song 
+      # Just in case the song doesn't exist in Echonest
       begin
 
-        key = Echowrap.song_search(:artist => song[:artist], :title => song[:title], :bucket => 'audio_summary').first.audio_summary.key
+        # Return metadata for song, pull out key and mode
+        metadata = Echowrap.song_search(:artist => song[:artist], :title => song[:title], :bucket => 'audio_summary').first.audio_summary
+        key = metadata.key
+        mode = metadata.mode
+
+        # Adjust saturation based on mode (major keys are lower saturation, minor keys are higher)
+        saturation = mode.zero? ? 128 : 254
 
         # Adjust hue based on key
         hue.lights.each do |light|
-          light.set_state(:xy => [colors[key][:x], colors[key][:y]])
+          light.set_state(:hue => colors[key][:hue], :brightness => colors[key][:bri], :saturation => saturation, :color_mode => "hs")
         end
 
       rescue
